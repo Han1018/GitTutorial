d7df6d37d1f7212d24864a0fbd4e2398b51ba196
Han1018 <t107590040@ntut.org.tw>
2020-03-13 22:55:59 +0800

    tutorial甲

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -4,14 +4,14 @@
 #include <iostream>
 using namespace System;
 bool Guess(int number) {
-	return false;
+	return 0;
 }
 int main(array<System::String ^> ^args)
 {
 	int guess;
 	do {
+		std::cout << "Choose a number between 1 - 100 :";
 		std::cin >> guess;
 	} while (!Guess(guess));
-	
 	return 0;
 }
