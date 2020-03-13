f702185b1eb2b8b540cdd4bb2c09bf3aee48160b
Han1018 <t107590040@ntut.org.tw>
2020-03-13 22:01:23 +0800

    tutorial7

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -1,10 +1,17 @@
-
 #include "pch.h"
+//#include "stdafx.h"
+#include <string>
 #include <iostream>
 using namespace System;
 bool Guess(int number) {
-	return 0;
+	return false;
 }
 int main(array<System::String ^> ^args)
 {
+	int guess;
+	do {
+		std::cin >> guess;
+	} while (!Guess(guess));
+	
+	return 0;
 }
